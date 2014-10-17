0e9202333d05ccac269b1fbf16e31a76e39cac0b
Veeranjaneyulu Sadhanala <veeranjaneyulus@gmail.com>
2014-10-17 02:25:05 -0400

    copying to R package

--- R_pkg/glmgen/src/tf/tf_admm.c
+++ R_pkg/glmgen/src/tf/tf_admm.c
@@ -41,7 +41,7 @@ void tf_admm (double * y, double * x, double * w, int n, int k, int family,
   D = tf_calc_dk(n, k+1, x);
   Dt = cs_transpose(D, 1);
   DDt = cs_multiply(D,Dt);
-  Dk = tf_calc_dk(n, k, x);
+  Dk = tf_calc_dktil(n, k, x);
   Dkt = cs_transpose(Dk, 1);
   DkDkt = cs_multiply(Dk,Dkt);
   DktDk = cs_multiply(Dkt,Dk);
@@ -120,12 +120,12 @@ void tf_admm (double * y, double * x, double * w, int n, int k, int family,
     {
       case FAMILY_GAUSSIAN:
         kernmat = scalar_plus_eye(DktDk, rho * lambda[i]);
-        kernmat_qr = glmgen_qr(kernmat);
-
+        kernmat_qr = glmgen_qr(kernmat);        
+        
         tf_admm_gauss(y, x, w, n, k, max_iter, lambda[i], beta+i*n, alpha,
                       u, obj+i*max_iter, iter+i, rho * lambda[i], obj_tol,
                       kernmat_qr);
-
+                      
         cs_spfree(kernmat);
         glmgen_gqr_free(kernmat_qr);
         break;
